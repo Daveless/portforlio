337c1864ac738c11953f89c6d9e90ceb4fa95062
David Rojas <44807207+Daveless@users.noreply.github.com>
2023-05-19 21:05:13 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
         <h3 className="text-2xl">David Rojas</h3>
         <p className="text-dav-border-gray">Web developer</p>
         <div className="text-3xl mt-2 flex gap-5">
-            <a href=""><i className='bx bxl-linkedin-square'></i></a>
-            <a href=""><i className='bx bxl-github' ></i></a>
-            <a href=""><i className='bx bx-envelope'></i></a>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/dave-rojas-villacreses/"><i className='bx bxl-linkedin-square'></i></a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/daveless/"><i className='bx bxl-github' ></i></a>
+            <a target="_blank" rel="noreferrer" href="mailto:davidalrojas@gmail.com"><i className='bx bx-envelope'></i></a>
         </div>
     </footer>
   )
